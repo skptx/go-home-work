c3b6c23ab0ffa6fefc371841143707a5e5db339b
Igor Cheledinov <iptx2104@gmail.com>
2025-04-18 18:17:10 +0400

    add new func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-home-work
+
+go 1.24.2
---
+++ homework_2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello from main")
+	myFunc()
+}
+
+func myFunc() {
+	fmt.Println("hello from my func!!")
+}
